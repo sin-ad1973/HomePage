d9ee870b56a9979c12c66dc866ac65606a2da7e5
sin-ad1973 <sin.ad1973@gmail.com>
2019-02-05 00:17:17 +0900

    test

---
+++ test/mochatest.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var myfunc = function(a, b) {
+    return a + b;
+}
+
+describe('テスト', function () {
+    it('myfuncのテスト', function () {
+        assert.strictEqual(myfunc(1, 2), 3);
+    });
+});
+
+
